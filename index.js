cb891530a7cca8e72cc21301588958d35f311f93
Thkeya <thkeyasmith@gmail.com>
2023-03-04 16:21:07 -0500

    Initial push to routes branch

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-//import your http
 const http = require("http")
+const { createDeflateRaw } = require("zlib")
+const fs = require("fs");
 
-//create server with HTTP
+
+
 const server = http.createServer((req, res)=> {
-console.log("Server is created")
-})
 
-//initiate a port
-const PORT = 2020;
+const url = req.url
+let tableData = "<table border=`1`><tr><th>name</th><th>height</th><th>birth_year</th><th>gender</th><th>url</th></tr>"
+if(url === '/list'){
+    fetch('https://swapi.dev/api/people')
+    .then(res => res.json())
+    .then(data => {
+        createData(data)
+        res.write(tableData)
+        res.end()
+    })
+   
+}
 
-//listen to server
-server.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+else if(url === '/'){
+    fs.readFile('springishere.jpg', function(error, file) {
+
+        var imagedata = Buffer.from(file).toString('base64');
+    
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write("<h1>Welcome</h1> <img src='data:springishere.jpg;base64,"+imagedata+"' width='400'/>");
+        res.end();
+    
+        });
+
+
+}
+
+
+
+else{
+    res.write("Page Not Found")
+    res.end()
+}
+
+
+function createData(data){
+    data.results.forEach(element => {
+        tableData+=`<tr><td>${element.name}</td><td>${element.height}</td><td>${element.birth_year}</td><td>${element.gender}</td><td>${element.url}</td></tr>`
+        
+    });
+    tableData+= `</table>`
+}
+
+}).listen(8090,console.log(`Server is listening on port 8090`))
